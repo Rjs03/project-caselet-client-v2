4c64a37717440f5bd5d560f82e123c2be475f02a
Ronak Sayta <M1046945@mindtree.com>
2019-07-23 17:07:27 +0530

    new pull

--- src/app/config/adalConfiguration.ts
+++ src/app/config/adalConfiguration.ts
@@ -5,8 +5,8 @@ export const adalConfig = {
   // clientId: 'd5324e3c-3c47-4053-b30c-08d5ca192eb9',
   // yorbit client id
   clientId: 'd5324e3c-3c47-4053-b30c-08d5ca192eb9',
-  redirectUri: 'http://localhost:4200',
-  // redirectUri: 'https://yorbitst.azurewebsites.net/projectcaselets/',
+  // redirectUri: 'http://localhost:4200',
+  redirectUri: 'https://yorbitst.azurewebsites.net/projectcaselets/',
   // redirectUri: 'https://yorbit.mindtree.com/projectcaselets/',
   postLogoutRedirectUri: window.location.origin,
   navigateToLoginResquestUrl: true,
